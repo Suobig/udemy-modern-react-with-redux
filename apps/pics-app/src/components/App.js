f1f517056fe9c1878f898f5ae9deb28f029326b1
Suobig <apopovwork@gmail.com>
2021-02-08 17:42:44 +0300

    Passed onSubmit from SearchBar back to App

--- apps/pics-app/src/components/App.js
+++ apps/pics-app/src/components/App.js
@@ -3,10 +3,14 @@ import SearchBar from "./SearchBar";
 import React from 'react';
 
 class App extends React.Component {
+  onSearchSumbit(term) {
+    console.log(`Got ${term} from a child`);
+  }
+
   render() {
     return (
       <div className="App ui container">
-        <SearchBar placeholder="Search images..." />
+        <SearchBar onSubmit={this.onSearchSumbit} placeholder="Search images..." />
       </div>
     );
   }
