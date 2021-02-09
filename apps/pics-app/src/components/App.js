5d22cd4577001bc28b9e5a25ffd472c6a07f8f42
Suobig <apopovwork@gmail.com>
2021-02-09 18:06:36 +0300

    Add rapidapi to search for images

--- apps/pics-app/src/components/App.js
+++ apps/pics-app/src/components/App.js
@@ -1,16 +1,28 @@
+import React from 'react';
+import rapidapi from '../api/rapidapi';
+
 import "./App.css";
 import SearchBar from "./SearchBar";
-import React from 'react';
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSumbit(term) {
-    console.log(`Got ${term} from a child`);
+  state = { images: []   }
+
+  onSearchSumbit = async (term) => {
+    const options = {
+      url: 'https://bing-image-search1.p.rapidapi.com/images/search',
+      params: {q: term}
+    };
+    
+    const response = await rapidapi.request(options);
+    this.setState({ images: response.data.value.map(it => it.thumbnailUrl) });
   }
 
   render() {
     return (
       <div className="App ui container">
         <SearchBar onSubmit={this.onSearchSumbit} placeholder="Search images..." />
+        <ImageList images = {this.state.images}/>
       </div>
     );
   }
