2ad80f9bd219a8a78cffdbdd960e5a99d8430e44
Попов Антон Сергеевич <ASergeevPopov@sberbank.ru>
2021-08-09 17:52:38 +0300

    create streams app

---
+++ apps/streams/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+const rootComponent = <App />
+
+ReactDOM.render(rootComponent, document.getElementById('root'))
